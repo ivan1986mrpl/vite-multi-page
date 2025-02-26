73ef60503dd86357b829d9b7d48c4efe759cee68
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-02-26 11:37:03 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,57 @@
+import SassGlob from 'vite-plugin-sass-glob-import';
+import { defineConfig } from 'vite';
+import { sync } from 'glob';
+import Inspect from 'vite-plugin-inspect';//https://github.com/antfu-collective/vite-plugin-inspect
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';//https://github.com/FatehAK/vite-plugin-image-optimizer
+
+const noAttr = () => {
+  return {
+    transformIndexHtml(html) {
+      return html.replaceAll(' crossorigin', '')
+    }
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    SassGlob(),
+    noAttr(),
+    Inspect(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 70,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 40,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 40,
+      },       
+      webp: {
+        quality: 40,					
+      }
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: sync('src/**/*.html'.replace(/\\/g, '/')),
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name
+          if (/css/.test(extType)) { extType = 'assets/css' }
+          return assetInfo.originalFileName ?? `${extType}/[name][extname]`
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    outDir: '../dist',
+  },
+  root: 'src',
+  base: '',
+})
